6e35f4d811542ae353f7ee47450e522d88219894
Jack Gamboa <jgamboa929@gmail.com>
2019-06-04 11:24:07 -0500

    shared.js - added event handlers for modal, backdrop.

--- sec-09-js-css/09-diving-deeper/shared.js
+++ sec-09-js-css/09-diving-deeper/shared.js
@@ -1,3 +1,23 @@
+const backdrop = document.querySelector(".backdrop");
 const modal = document.querySelector(".modal");
-// modal.style.display = "block";
-console.log(modal);
+const modalNoButton = document.querySelector(".modal__action--negative");
+const selectPlanButtons = document.querySelectorAll(".plan button");
+
+// console.dir(selectPlanButtons);
+// console.log(selectPlanButtons);
+
+for (let button of selectPlanButtons) {
+  button.addEventListener("click", () => {
+    modal.style.display = "block";
+    backdrop.style.display = "block";
+  });
+}
+
+modalNoButton.addEventListener("click", closeModal);
+
+backdrop.addEventListener("click", closeModal);
+
+function closeModal() {
+  backdrop.style.display = "none";
+  modal.style.display = "none";
+}
