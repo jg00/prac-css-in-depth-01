77239c9146ca79ed2af63fdafb205f86136b322b
Jack Gamboa <jgamboa929@gmail.com>
2020-04-23 12:40:29 -0500

    Add transitions to modal and backdrops.

--- sec-16-transitions/prac-01/16-uHost/shared.js
+++ sec-16-transitions/prac-01/16-uHost/shared.js
@@ -18,21 +18,33 @@ function closeModal() {
   if (modal) modal.classList.remove("open");
 
   backdrop.classList.remove("open");
+  setTimeout(function () {
+    backdrop.style.display = "none"; // For hacky solution to get opacity transition not be ignored.
+  }, 2000);
 }
 
 // Register button event listeners
 // Choose Plan
-selectPlanButtons.forEach(button => {
-  button.addEventListener("click", e => {
+selectPlanButtons.forEach((button) => {
+  button.addEventListener("click", (e) => {
     // modal.style.display = "block";
     // backdrop.style.display = "block";
 
     modal.classList.add("open");
-    backdrop.classList.add("open");
+
+    backdrop.style.display = "block"; // For hacky solution to get opacity transition not be ignored.
+    setTimeout(function () {
+      backdrop.classList.add("open");
+    }, 10);
   });
 });
 
-backdrop.addEventListener("click", function() {
+backdrop.addEventListener("click", function () {
+  backdrop.classList.remove("open");
+  setTimeout(function () {
+    backdrop.style.display = "none"; // For hacky solution to get opacity transition not be ignored.
+  }, 2000);
+
   // mobileNav.style.display = "none";
   mobileNav.classList.remove("open");
   closeModal();
@@ -41,12 +53,16 @@ backdrop.addEventListener("click", function() {
 if (modalNoButton) modalNoButton.addEventListener("click", closeModal);
 
 // Mobile Nav
-toggleButton.addEventListener("click", e => {
+toggleButton.addEventListener("click", (e) => {
   // One approach
   // mobileNav.style.display = "block";
   // backdrop.style.display = "block";
 
   // Alternative
   mobileNav.classList.add("open");
-  backdrop.classList.add("open");
+
+  backdrop.style.display = "block"; // For hacky solution to get opacity transition not be ignored.
+  setTimeout(function () {
+    backdrop.classList.add("open");
+  }, 10);
 });
